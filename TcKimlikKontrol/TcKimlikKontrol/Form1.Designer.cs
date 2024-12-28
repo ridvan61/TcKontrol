da3d6e22186f1ba41c6f32e6c94ac3aa23b61c62
Rıdvan Öztürk <ozturkridvan51@gmail.com>
2024-12-28 12:56:27 +0300

    Tc Kimlik Kontrol
    Tc algoritmasıyla girilen 11 haneli numaranın uyumluluğunu kontrol eder

---
+++ TcKimlikKontrol/TcKimlikKontrol/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace TcKimlikKontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txt_GonderenTCno = new System.Windows.Forms.TextBox();
+            this.TCHatasi = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.TCHatasi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_GonderenTCno
+            // 
+            this.txt_GonderenTCno.Location = new System.Drawing.Point(279, 190);
+            this.txt_GonderenTCno.Name = "txt_GonderenTCno";
+            this.txt_GonderenTCno.Size = new System.Drawing.Size(185, 20);
+            this.txt_GonderenTCno.TabIndex = 0;
+            this.txt_GonderenTCno.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txt_GonderenTCno.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTcKimlik_KeyPress_1);
+            this.txt_GonderenTCno.Leave += new System.EventHandler(this.txt_GonderenTCno_Leave);
+            // 
+            // TCHatasi
+            // 
+            this.TCHatasi.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_GonderenTCno);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.TCHatasi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_GonderenTCno;
+        private System.Windows.Forms.ErrorProvider TCHatasi;
+    }
+}
+
---
+++ TcKimlikKontrol/TcKimlikKontrol/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TcKimlikKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+          
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            string tck = txt_GonderenTCno.Text;
+            if (TCkontrolü(tck))
+            { TCHatasi.Clear(); }
+            else if (tck == "")
+            { TCHatasi.Clear(); }
+
+
+        }
+
+        private bool TCkontrolü(string TCtext)
+        {
+            string tcKimlik = txt_GonderenTCno.Text;
+
+            // TC kimlik numarası 11 haneli olmalı ve sadece sayılardan oluşmalı
+            if (tcKimlik.Length != 11)
+            {
+                return false;
+            }
+
+            // İlk hanesi 0 olamaz
+            if (tcKimlik[0] == '0')
+            {
+                TCHatasi.SetError(txt_GonderenTCno, "TC Kimlik numarasının ilk hanesi 0 olamaz.");
+                return false;
+            }
+
+            // İlk 9 haneyi al
+            int[] digits = tcKimlik.Take(9).Select(x => int.Parse(x.ToString())).ToArray();
+
+            // 10. hane kontrolü (tek ve çift hanelerin kontrolü)
+            int toplamTek = digits[0] + digits[2] + digits[4] + digits[6] + digits[8];
+            int toplamCift = digits[1] + digits[3] + digits[5] + digits[7];
+            int hane10 = ((toplamTek * 7) - toplamCift) % 10;
+
+            if (hane10 != int.Parse(tcKimlik[9].ToString()))
+            {
+                TCHatasi.SetError(txt_GonderenTCno, "10.Basamak Hatası.");
+                return false;
+            }
+
+            // İlk 10 hanenin toplamının mod 10'u 11. haneyi vermeli
+            int toplamIlk10 = digits.Sum() + hane10;
+            int hane11 = toplamIlk10 % 10;
+
+            if (hane11 != int.Parse(tcKimlik[10].ToString()))
+            {
+                TCHatasi.SetError(txt_GonderenTCno, "11.Basamak Hatası.");
+                return false;
+            }
+
+            // Eğer tüm kontroller geçildiyse, TC kimlik numarası geçerlidir ve hata temizlenir
+            TCHatasi.SetError(txt_GonderenTCno, "");
+            return true;
+        }
+
+        private void txtTcKimlik_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            //sadece rakam girilebilir
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+            // Eğer basılan tuş 0 ise ve TextBox boşsa, girişi engelliyoruz
+            if (e.KeyChar == '0' && txt_GonderenTCno.Text.Length == 0)
+            {
+                e.Handled = true;
+            }
+            if (txt_GonderenTCno.Text.Length == 11 && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+        }
+        private void txt_GonderenTCno_Leave(object sender, EventArgs e)
+        {
+            string kont = txt_GonderenTCno.Text;
+            if (kont.Length != 11)
+            { TCHatasi.SetError(txt_GonderenTCno, "11 Hane Olmak Zorunda"); return; }
+            else if (kont == "")
+            { TCHatasi.SetError(txt_GonderenTCno, "Boş Bırakılamaz."); return; }
+            else
+            { TCHatasi.Clear(); }
+        }
+    }
+}
